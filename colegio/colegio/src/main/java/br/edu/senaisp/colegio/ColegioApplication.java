16e88b35088507ddb9a162fe9b863f72b62fcd4b
bit-senai <bit_senai@outlook.com>
2025-04-08 19:15:48 -0300

    Versao02
    Versao02

---
+++ colegio/colegio/src/main/java/br/edu/senaisp/colegio/ColegioApplication.java
@@ -0,0 +1,15 @@
+package br.edu.senaisp.colegio;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColegioApplication.class, args);
+	}
+}
