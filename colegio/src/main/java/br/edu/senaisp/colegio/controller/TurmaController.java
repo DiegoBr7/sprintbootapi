78f025473ec0609fbf69e1aa5a02985e569bac1b
bit-senai <bit_senai@outlook.com>
2025-04-15 19:08:23 -0300

    Version 15-04-2025

---
+++ colegio/src/main/java/br/edu/senaisp/colegio/controller/TurmaController.java
@@ -0,0 +1,113 @@
+package br.edu.senaisp.colegio.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.senaisp.colegio.model.Aluno;
+import br.edu.senaisp.colegio.model.Turma;
+import br.edu.senaisp.colegio.repository.TurmaRepository;
+import br.edu.senaisp.colegio.service.TurmaService;
+
+@RestController
+@RequestMapping("/api/turma")
+public class TurmaController {
+
+	@Autowired
+	private TurmaService turmaService;
+	
+	@Autowired
+	private TurmaRepository repoTurma;
+
+	@GetMapping("/jpa")
+	public String testeJPA() {
+		List<Turma> lista = new ArrayList<Turma>();
+		
+		//List<Turma> t = repoTurma.findByNome("Font-End com React");
+
+		//long qtd = repoTurma.count();
+		
+		//long qtd = repoTurma.countByNome("Font-End com React");
+		//System.err.println(qtd);
+		
+		//List<Turma> lista = repoTurma.findByNomeNotContains("Font");
+		
+		//Optional<Turma> op = repoTurma.findFirstByNome("Font-End com React");
+		 
+		//System.out.println(op.orElse(null).getNome());
+		
+		lista = repoTurma.findByNomeAndId("Font-End com React", 15L);
+		
+		for (Turma turma : lista) {
+			System.err.println(turma.getNome());
+		}
+		return "Foi!";
+	}
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Turma>> buscarTodos() {
+		return ResponseEntity.ok(turmaService.buscarTodos());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Turma> buscarPorId(@PathVariable Long id) throws IOException {
+		Turma t = turmaService.buscarPorId(id);
+
+		return ResponseEntity.ok(t);
+	}
+
+	@PostMapping
+	public ResponseEntity inserir(@RequestBody Turma turma) {
+
+		try {
+			Turma t = turmaService.gravarTurma(turma);
+			return ResponseEntity.ok(t);
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity alterar(@PathVariable Long id, @RequestBody Turma turma) {
+
+		try {
+			Turma t = turmaService.alterarPorId(id, turma);
+			return ResponseEntity.ok(t);
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity excluirPorId(@PathVariable Long id) {
+		try {
+			Turma t = turmaService.excluirPorId(id);
+
+			if (t == null)
+				return ResponseEntity.notFound().build();
+			else
+				return ResponseEntity.ok(t);
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
+	}
+
+}
